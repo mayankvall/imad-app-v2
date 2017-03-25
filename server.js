1772fe6538d6c5846a09a4d578f0d23ad403d4ea
Mayank <mayankv@gmail.com>
2017-03-25 13:21:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,9 +17,8 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-var server = app.listen(8080, function () {
-   var host = server.address().address
-   var port = server.address().port
+app.listen(8080, function () {
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   
+   //console.log("Example app listening at http://%s:%s", host, port)
 })
