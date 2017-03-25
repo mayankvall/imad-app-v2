0514cfb1963f46443826e384372778cc21d32d4b
Mayank <mayankv@gmail.com>
2017-03-25 15:24:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,39 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
+var pages={
+  'pages-one':{
+      title:"pages-one",
+      date:"26th sep 2017",
+      content:`<p>hello world
+      </p>
+      <p>
+        I am back
+      </p>
+      `
+  },
+};
+function template(data)
+{
+    var title=data.title;
+    var date=data.date;
+    var content=data.content;
+    var htmltemplate=`
+    <html>
+        <body>
+            <div>title:${title}</div>
+            <div>date:${date}</div>
+            <div>${content}</div>
+        </body>
+    </html>
+`
+    
+}
+
+app.get('/:pagesname',function(req,res){
+    var pagesname=req.params.pagesname;
+   res.sendFile(template(pagesname)); 
+});
 app.listen(8080, function () {
    
    
